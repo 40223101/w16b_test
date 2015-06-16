008ceb5e542b97aec1e602851575189504be7a5b
chiaming yen <chiamingyen@gmail.com>
2015-06-16 11:23:09 +0800

    40223101

---
+++ wsgi.py
@@ -0,0 +1,194 @@
+# coding=utf-8
+# 上面的程式內容編碼必須在程式的第一或者第二行才會有作用
+
+################# (1) 模組導入區
+# 導入 cherrypy 模組, 為了在 OpenShift 平台上使用 cherrypy 模組, 必須透過 setup.py 安裝
+
+
+
+import cherrypy
+# 導入 Python 內建的 os 模組, 因為 os 模組為 Python 內建, 所以無需透過 setup.py 安裝
+import os
+# 導入 random 模組
+import random
+import math
+from cherrypy.lib.static import serve_file
+# 導入 gear 模組
+#import gear
+import man
+import man2
+
+################# (2) 廣域變數設定區
+# 確定程式檔案所在目錄, 在 Windows 下有最後的反斜線
+_curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
+# 設定在雲端與近端的資料儲存目錄
+if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+    # 表示程式在雲端執行
+    download_root_dir = os.environ['OPENSHIFT_DATA_DIR']
+    data_dir = os.environ['OPENSHIFT_DATA_DIR']
+else:
+    # 表示程式在近端執行
+    download_root_dir = _curdir + "/local_data/"
+    data_dir = _curdir + "/local_data/"
+
+
+def downloadlist_access_list(files, starti, endi):
+    # different extension files, associated links were provided
+    # popup window to view images, video or STL files, other files can be downloaded directly
+    # files are all the data to list, from starti to endi
+    # add file size
+    outstring = ""
+    for index in range(int(starti)-1, int(endi)):
+        fileName, fileExtension = os.path.splitext(files[index])
+        fileExtension = fileExtension.lower()
+        fileSize = sizeof_fmt(os.path.getsize(download_root_dir+"downloads/"+files[index]))
+        # images files
+        if fileExtension == ".png" or fileExtension == ".jpg" or fileExtension == ".gif":
+            outstring += '<input type="checkbox" name="filename" value="'+files[index]+'"><a href="javascript:;" onClick="window.open(\'/downloads/'+ \
+            files[index]+'\',\'images\', \'catalogmode\',\'scrollbars\')">'+files[index]+'</a> ('+str(fileSize)+')<br />'
+        # stl files
+        elif fileExtension == ".stl":
+            outstring += '<input type="checkbox" name="filename" value="'+files[index]+'"><a href="javascript:;" onClick="window.open(\'/static/viewstl.html?src=/downloads/'+ \
+            files[index]+'\',\'images\', \'catalogmode\',\'scrollbars\')">'+files[index]+'</a> ('+str(fileSize)+')<br />'
+        # flv files
+        elif fileExtension == ".flv":
+            outstring += '<input type="checkbox" name="filename" value="'+files[index]+'"><a href="javascript:;" onClick="window.open(\'/flvplayer?filepath=/downloads/'+ \
+            files[index]+'\',\'images\', \'catalogmode\',\'scrollbars\')">'+files[index]+'</a> ('+str(fileSize)+')<br />'
+        # direct download files
+        else:
+            outstring += "<input type='checkbox' name='filename' value='"+files[index]+"'><a href='/download/?filepath="+download_root_dir.replace('\\', '/')+ \
+            "downloads/"+files[index]+"'>"+files[index]+"</a> ("+str(fileSize)+")<br />"
+    return outstring
+def sizeof_fmt(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+################# (3) 程式類別定義區
+# 以下改用 CherryPy 網際框架程式架構
+# 以下為 Hello 類別的設計內容, 其中的 object 使用, 表示 Hello 類別繼承 object 的所有特性, 包括方法與屬性設計
+class Midterm(object):
+
+    # Midterm 類別的啟動設定
+    _cp_config = {
+    'tools.encode.encoding': 'utf-8',
+    'tools.sessions.on' : True,
+    'tools.sessions.storage_type' : 'file',
+    #'tools.sessions.locking' : 'explicit',
+    # session 以檔案儲存, 而且位於 data_dir 下的 tmp 目錄
+    'tools.sessions.storage_path' : data_dir+'/tmp',
+    # session 有效時間設為 60 分鐘
+    'tools.sessions.timeout' : 60
+    }
+
+    def __init__(self):
+        # hope to create downloads and images directories　
+        if not os.path.isdir(download_root_dir+"downloads"):
+            try:
+                os.makedirs(download_root_dir+"downloads")
+            except:
+                print("mkdir error")
+        if not os.path.isdir(download_root_dir+"images"):
+            try:
+                os.makedirs(download_root_dir+"images")
+            except:
+                print("mkdir error")
+        if not os.path.isdir(download_root_dir+"tmp"):
+            try:
+                os.makedirs(download_root_dir+"tmp")
+            except:
+                print("mkdir error")
+    # 以 @ 開頭的 cherrypy.expose 為 decorator, 用來表示隨後的成員方法, 可以直接讓使用者以 URL 連結執行
+    @cherrypy.expose
+    # index 方法為 CherryPy 各類別成員方法中的內建(default)方法, 當使用者執行時未指定方法, 系統將會優先執行 index 方法
+    # 有 self 的方法為類別中的成員方法, Python 程式透過此一 self 在各成員方法間傳遞物件內容
+    def index(self):
+        outstring = '''
+        <!DOCTYPE html> 
+        <html>
+        <head>
+        
+        </head>
+        <body>
+        <a href="a_40223101">a_40223101</a><br />
+        
+        </body>
+        </html>
+        '''
+        
+        return outstring
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def a_40223101(self):
+        outstring = '''
+        <!DOCTYPE html> 
+        <html>
+        <head>
+        40223101
+        </head>
+        <body>
+        <br /><a href="index">index</a><br />
+        </body>
+        </html>
+        '''
+
+        return outstring
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def spur(self, N=20, M=5, P=15):
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.3-20150514-095342/brython.js"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">
+        
+    <form method=POST action=spuraction>
+    齒數:<input type=text name=N value='''+str(N)+'''><br />
+    模數:<input type=text name=M value = '''+str(M)+'''><br />
+    壓力角:<input type=text name=P value = '''+str(P)+'''><br />
+    <input type=submit value=send>
+    </form>
+    <br /><a href="index">index</a><br />
+    </body>
+    </html>
+    '''
+
+        return outstring
+class Download:
+    @cherrypy.expose
+    def index(self, filepath):
+        return serve_file(filepath, "application/x-download", "attachment")
+################# (4) 程式啟動區
+# 配合程式檔案所在目錄設定靜態目錄或靜態檔案
+application_conf = {'/static':{
+        'tools.staticdir.on': True,
+        # 程式執行目錄下, 必須自行建立 static 目錄
+        'tools.staticdir.dir': _curdir+"/static"},
+        '/downloads':{
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': data_dir+"/downloads"},
+        '/images':{
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': data_dir+"/images"}
+    }
+    
+root = Midterm()
+root.download = Download()
+root.man = man.MAN()
+root.man2 = man2.MAN()
+
+#root.gear = gear.Gear()
+
+if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+    # 表示在 OpenSfhit 執行
+    application = cherrypy.Application(root, config=application_conf)
+else:
+    # 表示在近端執行
+    cherrypy.config.update({'server.socket_port':8099})
+    cherrypy.quickstart(root, config=application_conf)
